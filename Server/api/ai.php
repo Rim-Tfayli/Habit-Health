0e6786264d0ec18055ea977fac43f477def304b4
Rim-Tfayli <Rim.tf.2004@hotmail.com>
2025-11-14 09:22:22 +0200

    backend: add user input to the db; take ai response and add it to th db

--- Server/api/ai.php
+++ Server/api/ai.php
@@ -1,17 +1,28 @@
 <?php 
     require_once("../connection/connection.php");
     require_once("../services/ResponseService.php");
+    require_once("../services/EntryService.php");
+    require_once("../services/AiResponseService.php");
 
+    //will be sent from axios..
     $data = json_decode(file_get_contents("php://input"), true);
     if (!isset($data['user_id']) || !isset($data['user_input'])){
         echo ResponseService::response(400, "Some info are missing");
         exit;
     }
+    $entry_id = $data["entry_id"];
     $userId = $data['user_id'];
     $userInput = $data['user_input'];
+
+    $newEntry = EntryService::save($connection, [
+        "id" => $entry_id,
+        "user_id" => $user_id,
+        "entry_text" => $userInput,
+        "created_at" => date('Y-m-d H:i:s')
+    ]);
     $apiKey = "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 ";
 
-    $prompt = "will see \"$userText\"";
+    $prompt = "will see \"$userInput\"";
     
     $msg = [
         'model' => 'gpt-3.5-turbo',
@@ -44,9 +55,18 @@
         echo json_encode(["status" => "failed", "message" => "Invalid JSON response from OpenAI"]);
         return;
     }
+    $aiResponse = AiResponseService::save($connection, [
+        "entry_id" => $entry_id,
+        "steps" => $resultData['steps'],
+        "minutes" => $resultData['minutes'],
+        "caffeine" => $resultData['caffeine'],
+        "sleep_time"=> $resultData['sleep_time'],
+        "calories" => $resultData['calories']
+    ]);
 
-    //CRUD ymkn
-
+    if($aiResponse){
+        echo ResponseService::response(200, "Entry and AI Response saved");
+    }
     
 
 
