5da02faaedbfb552e60fd4b39fa16514fa642052
Rim-Tfayli <Rim.tf.2004@hotmail.com>
2025-11-14 06:56:11 +0200

    frontend: login-signup fixes

--- Client/script/login.js
+++ Client/script/login.js
@@ -1,34 +1,36 @@
 document.getElementById('submit-login').onclick = function(e){
     e.preventDefault();
 
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
     
     const emailError = document.getElementById('email-error');
     const passwordError = document.getElementById('password-error');
 
     let valid = true;
 
-    if(!isValidEmail(emailInput.value.trim())){
+    if(!isValidEmail(email)){
         emailError.textContent = 'Invalid email format';
         valid = false;
     }
-    if(passwordInput.value.length < 6){
+    if(password.length < 6){
         passwordError.textContent = 'Password must be at least 6 characters';
         valid = false;
     }
 
     if (!valid) return;
 
-    login(email);
+    login(email, password);
 };
-async function createNewUser(data){
+async function login(email, password){
     try{
-        const response = await axios.get(`${BASE_URL}/user`, {
-            params:{ email: email }
+        const response = await axios.post(`${BASE_URL}/user/login`, {
+            email: email,
+            password: password
         });
         if(response){
-            localStorage.setItem('email', emailInput.value.trim());
+            localStorage.setItem('email', email);
+            window.location.href="/dashboard.html";
             return response.data;
         }
     }
--- Client/script/signup.js
+++ Client/script/signup.js
@@ -1,11 +1,11 @@
 document.getElementById('submit-signup').onclick = function(e){
     e.preventDefault();
 
-    const username = document.getElementById('username');
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    const confirm = document.getElementById('confirm');
-    const gender = document.getElementById('gender');
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value;
+    const confirm = document.getElementById('confirm').value;
+    const gender = document.getElementById('gender').value;
     
     const usernameError = document.getElementById('usernam-error');
     const emailError = document.getElementById('email-error');
@@ -15,23 +15,23 @@ document.getElementById('submit-signup').onclick = function(e){
 
     let valid = true;
 
-    if(username.value.trim() === ''){
+    if(username === ''){
         usernameError.textContent = 'Username is required';
         valid = false;
     }
-    if(!isValidEmail(emailInput.value.trim())){
+    if(!isValidEmail(email)){
         emailError.textContent = 'Invalid email format';
         valid = false;
     }
-    if(passwordInput.value.length < 6){
+    if(password < 6){
         passwordError.textContent = 'Password must be at least 6 characters';
         valid = false;
     }
-    if(passwordInput.value !== confirmInput.value){
+    if(password !== confirmInput.value){
         confirmError.textContent = 'Passwords do not match';
         valid = false;
     }
-    if (!genderInput.value) {
+    if (!gender) {
         genderError.textContent = 'Please select your gender';
         valid = false;
     }
@@ -39,18 +39,19 @@ document.getElementById('submit-signup').onclick = function(e){
     if (!valid) return;
 
     const data = {
-        username: username.value.trim(),
-        email: email.value.trim(),
-        password: password.value,
-        gender: gender.value
+        username: username,
+        email: email,
+        password: password,
+        gender: gender
     };
-    createNewUser(data);
-
-    localStorage.setItem('email', emailInput.value.trim());
+    const signup =createNewUser(data);
+    if(signup)
+        localStorage.setItem('email', email);
 };
 async function createNewUser(data){
     try{
         const response = await axios.post(`${BASE_URL}/user/insert`, data);
+        window.location.href="/dashboard.html";
         return response.data;
     } 
     catch(error){
