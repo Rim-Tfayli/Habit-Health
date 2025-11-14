c0c41a61c4f11ab778e16fed4d14efe000310a9b
Rim-Tfayli <Rim.tf.2004@hotmail.com>
2025-11-14 19:17:14 +0200

    backend: weekly & daily prompot

--- Server/api/prompts.php
+++ Server/api/prompts.php
@@ -1,10 +1,53 @@
 <?php 
+$prompt1 = <<<EOT
+            Act like a habit health logger app, I send you entries like (Walked 25 min, 2 coffees, slept at 01:30)
+            and you return parse it into structured data (steps, water, caffeine, sleep_time, calories)
+            Estimate steps if only walking minutes are provided (1min = 100step)
+            And when someone tell you what he ate, you calculate calories
 
-$prompt1 = "";
+            Your json response should loook like:
+            {
+                "steps": ..,
+                "water": ..,
+                "caffeine": ..,
+                "sleep_time": "..",
+                "calories": ..
+            }
+            Do not add any text outside the JSON object.
+        EOT;
 
-$prompt2 = "";
+$prompt2 = <<<EOT
+                Act like a nutrition coach that analyzes habits and entries carefully.
+                And when someone tell you what he ate, you calculate calories
+                (I send you my habits and daily entries)
+                Based on that, generate a JSON response only.
+                -Parse the entries into structured data: steps, water (liters), sleep (hours), calories.
+                -Estimate steps if only walking minutes are provided (100 steps ≈ 1 minute).
+                -Compare the parsed data with the habits' goals.
+                -feedback should only be one sentence
+                -Return a JSON containing:
+                {
+                "summary": {
+                    "steps": ..,
+                    "water_liters": ..,
+                    "sleep_hours": ..,
+                    "calories": ..
+                },
+                "gaps": {
+                    "steps_gap": ...,
+                    "water_gap_liters": ..,
+                    "sleep_gap_hours": ..,
+                    "calories_gap": ..
+                },
+                "feedback": [
+                    "string feedback items..."
+                ]
+                }
+                Do not add any text outside the JSON object
+            EOT;
 
-$prompt3 = `Act like a nutrition coach who has online clients,
+$prompt3 = <<<EOT
+            Act like a nutrition coach who has online clients,
             I am your online client and I am keeping you updated on my weekly processso I can have healthy lifestyle.
             First, give me my weekly summary, just by doing your calculations.
             Then send me an encouraging message to help me stay commited.
@@ -30,6 +73,8 @@ $prompt3 = `Act like a nutrition coach who has online clients,
                     "advice2": "...",
                     ....
                 }               
-            }`
+            }
+            Do not add any text outside the JSON object.
+        EOT;
 
 ?>
