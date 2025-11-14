3b39599f2abd9aeb354fbb228fc4be7df53ac76a
Rim-Tfayli <Rim.tf.2004@hotmail.com>
2025-11-14 17:44:14 +0200

    backend: prompt for weekly summary

--- Server/api/prompts.php
+++ Server/api/prompts.php
@@ -6,9 +6,30 @@ $prompt2 = "";
 
 $prompt3 = `Act like a nutrition coach who has online clients,
             I am your online client and I am keeping you updated on my weekly processso I can have healthy lifestyle.
-            First, send me an encouraging message to help me stay commited.
-            Then give me a list of advices like (you should drink more water, you should eat more protein..).
-            Note that all this is based on the data I sent you below.
-            `
+            First, give me my weekly summary, just by doing your calculations.
+            Then send me an encouraging message to help me stay commited.
+            And finally give me a list of advices like(you should drink more water, you should eat more protein..) based on the summary.
+            
+            Dont say anything else, only short message, then very short advices           
+            All this is based on the data I sent you below.
+            Note that my data contains random informations (about what I ate, drank... and about my sleep) at different times on different days.
+            So to know each day's data, you need to aggregate it based on the "created_at field
+            Your response should look like:
+                {
+                "summary": {
+                    "steps": "...",
+                    "water": "...",
+                    "caffeine": "..",
+                    "sleep_time": "..."
+                }
+                "message": [
+                    "Your encouraging message"
+                ],
+                "advices": {
+                    "advice1": "You should ...",
+                    "advice2": "...",
+                    ....
+                }               
+            }`
 
 ?>
--- Server/models/Entry.php
+++ Server/models/Entry.php
@@ -13,11 +13,14 @@ class Entry extends Model {
     public function __construct(array $data){
         $this->user_id = (int)$data["user_id"];
         $this->entry_text = $data["entry_text"];
+        $this->created_at = $data["created_at"];
     }
     public function toArray(){
         return [
             "user_id" => $this->user_id,
             "entry_text" => $this->entry_text,
+            "created_at" => $this->created_at
+
         ];
     }
 
