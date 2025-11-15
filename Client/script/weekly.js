d6c4cd52993923ea9560abd8ef00282d0bd99c4f
Rim-Tfayli <Rim.tf.2004@hotmail.com>
2025-11-15 21:21:00 +0200

    frontend: tested weekly summary

--- Client/script/weekly.js
+++ Client/script/weekly.js
@@ -1,7 +1,8 @@
 window.onload = function() {
-  getHabits();
+ // getHabits();
+  
 };
-async function getHabits(){
+/*async function getHabits(){
      try{
         console.log(schema);
         const res = await axios.post(`${BASE_URL}/api/weeklySummary.php`,{
@@ -24,15 +25,35 @@ async function getHabits(){
         console.error("Error!", error);
         return {status: 500, data: 'connection failed'};
     }
+}*/
+adv = {        
+    "summary": {
+        "steps": "10500",
+        "water": "2.5",
+        "caffeine": "70",
+        "sleep_time": "4"
+    },
+    "message": [
+        "Ahsante ya bannut keep going you can do it"
+    ],
+    "advices": {
+        "adv1": "You should drink more water.",
+        "adv2": "You should balance caffeine intake.",
+        "adv3": "You should sleep more",
+        "adv4": "You should include more protein in your meals.",
+        "adv5": "You shouldmove more."
+    }
 }
+message(adv.message);
+advices(adv.advices);
 function message(msg){
     const message = document.getElementById("message");
-    message.innerHTML(msg);
+    message.innerHTML=msg;
 }
 function advices(adv){
     const advices = document.getElementById("advices");
-    let advs = `<h1>For a better lifestyle: <i class="fa-solid fa-heart-circle-plus"></i></h1><ul>`;
-    adv.forEach(advice => {
+    let advs = `<h1>Advices to improve your Lifestyle <i class="fa-solid fa-heart-circle-plus"></i></h1><ul>`;    
+    Object.values(adv).forEach(advice => {
         advs += `<li> ${advice}'</li>`;        
     });
     advs +=`</ul>`;
