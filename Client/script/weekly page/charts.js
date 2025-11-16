6d3b79af6416d3b60df93fbe7514f97c7a398821
Rim-Tfayli <Rim.tf.2004@hotmail.com>
2025-11-16 18:26:58 +0200

    frontend: preparing for weekly charts

--- Client/script/weekly page/charts.js
+++ Client/script/weekly page/charts.js
@@ -12,6 +12,46 @@ async function displayCharts() {
     }
     
 }
-function displayWeeklyChart(data){
-    
+const resp = {
+    "status": 200,
+    "data": [
+        {
+            "id": null,
+            "user_id": 1,
+            "steps": 10500,
+            "water": 3,
+            "caffeine": 2,
+            "sleep_time": "0",
+            "calories": 2200
+        },
+        {
+            "id": null,
+            "user_id": 1,
+            "steps": 554,
+            "water": 3,
+            "caffeine": 5,
+            "sleep_time": "0",
+            "calories": 1000
+        }
+    ]
+};
+prepareData(resp.data); 
+function prepareData(data){ 
+    let water = []; 
+    let steps = []; 
+    let caffeine = [];
+    let sleep_time = []; 
+    let calories = []; 
+    data.forEach(val => { 
+        water.push(val.water); 
+        steps.push(val.steps); 
+        caffeine.push(val.caffeine); 
+        sleep_time.push(val.sleep_time); 
+        calories.push(val.calories); 
+    });
+    console.log(water);
+    drawCharts(water, steps, caffeine, sleep_time, calories); 
+}
+function drawCharts(water, steps, caffeine, sleep_time, calories){
+
 }
